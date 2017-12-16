172d59762bc60e9681b8db969b2fc4ba97c97bfb
Nerijus <dnerijus72@gmail.com>
2017-12-16 16:06:49 +0200

    Define style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './styles/App.css';
 
 import Calendar from './components/calendar';
+import Sidepanel from './components/sidepanel';
 
 export default () => (
   <div className="App">
     <Calendar />
-    <div className="side-panel">Sidepanel placeholder</div>
+    <Sidepanel />
   </div>
 );
---
+++ src/components/sidepanel.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Sidepanel extends React.Component {  
+  render() {
+    return (
+      <div className="sidepanel">
+        <div className="sidepanel-info">
+          <div>Tuesday</div>
+          <div>15th December 2017</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidepanel;
